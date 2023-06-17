082d3b1439c9989a83fcbd30adba1c1618ce5005
rainforce15 <rainforce15@gmail.com>
2023-06-17 21:31:06 +0200

    - converted the PatternVisitor back into an inner class - added configuration window entries - restricted the IntelliJ Versions compatible with the new config implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.aeolid"
-version = "1.2"
+version = "1.3b"
 
 repositories {
     mavenCentral()
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2.5")
+    version.set("2023.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf("com.intellij.java"))
@@ -32,8 +32,8 @@ tasks {
 
     patchPluginXml {
         version.set("${project.version}")
-        sinceBuild.set("222")
-        untilBuild.set("232.*")
+        sinceBuild.set("231")
+        untilBuild.set("242.*")
     }
 
     signPlugin {
--- src/main/kotlin/com/aeolid/generatorproofing/InspectionImpl.kt
+++ src/main/kotlin/com/aeolid/generatorproofing/InspectionImpl.kt
@@ -1,16 +1,58 @@
 package com.aeolid.generatorproofing
 
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool
+import com.intellij.codeInspection.ProblemDescriptorBase
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInspection.options.OptPane
+import com.intellij.codeInspection.options.OptPane.*
+import com.intellij.psi.JavaElementVisitor
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiJavaFile
 
 class InspectionImpl: AbstractBaseJavaLocalInspectionTool() {
+	var headerPattern: String = "/* Generated File */"
+	var beginPattern: String = "// IMPLEMENTATION BEGIN"
+	var endPattern: String = "// IMPLEMENTATION END"
 
 	override fun runForWholeFile(): Boolean {
 		return true
 	}
 
+	override fun getOptionsPane(): OptPane {
+		return pane(
+			string("headerPattern", InspectionBundle.getMessage("inspection.generatedCodePattern.display.headerPattern")),
+			string("beginPattern", InspectionBundle.getMessage("inspection.generatedCodePattern.display.beginPattern")),
+			string("endPattern", InspectionBundle.getMessage("inspection.generatedCodePattern.display.endPattern"))
+		)
+	}
+
 	override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
 		return GeneratorPatternVisitor(holder)
+	}
+
+	inner class GeneratorPatternVisitor(private val holder: ProblemsHolder): JavaElementVisitor() {
+		private val _errorText = InspectionBundle.getMessage("inspection.generatedCodePattern.display.name")
+
+		override fun visitJavaFile(file: PsiJavaFile) {
+			if (file.firstChild.text.contains(headerPattern)) {
+				for (changedRange in getAffectedRanges(file, beginPattern, endPattern)) {
+					holder.registerProblem(
+						ProblemDescriptorBase(
+						file,
+						file,
+						_errorText,
+						null,
+						ProblemHighlightType.ERROR,
+						false,
+						changedRange,
+						true,
+						false
+					)
+					)
+				}
+			}
+			super.visitJavaFile(file)
+		}
 	}
 }
