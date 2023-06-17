91d1b3fbafa262f80dfd5994779972f1722dc113
rainforce15 <rainforce15@gmail.com>
2023-06-17 22:05:17 +0200

    config update for signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,48 @@
 plugins {
-    id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
-    id("org.jetbrains.intellij") version "1.13.3"
+	id("java")
+	id("org.jetbrains.kotlin.jvm") version "1.8.21"
+	id("org.jetbrains.intellij") version "1.13.3"
 }
 
 group = "com.aeolid"
 version = "1.3b"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.1")
-    type.set("IC") // Target IDE Platform
+	version.set("2023.1")
+	type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf("com.intellij.java"))
+	plugins.set(listOf("com.intellij.java"))
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
+	// Set the JVM compatibility versions
+	withType<JavaCompile> {
+		sourceCompatibility = "17"
+		targetCompatibility = "17"
+	}
+	withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+		kotlinOptions.jvmTarget = "17"
+	}
 
-    patchPluginXml {
-        version.set("${project.version}")
-        sinceBuild.set("231")
-        untilBuild.set("242.*")
-    }
+	patchPluginXml {
+		version.set("${project.version}")
+		sinceBuild.set("231")
+		untilBuild.set("242.*")
+	}
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
+	signPlugin {
+		certificateChainFile.set(file("../jbMarketData/chain.crt"))
+		privateKeyFile.set(file("../jbMarketData/private.pem"))
+		password.set(providers.environmentVariable("PRIVATE_KEY_PASSWORD"))
+	}
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
+	publishPlugin {
+		token.set(providers.environmentVariable("PUBLISH_TOKEN"))
+	}
 }
