f5d420e3dd09d5365afb7653c5e56342ff8a2abf
rainforce15 <rainforce15@gmail.com>
2023-06-17 12:25:20 +0200

    converted Utilities to Kotlin

---
+++ src/main/kotlin/com/aeolid/generatorproofing/Utilities.kt
@@ -0,0 +1,139 @@
+@file:JvmName("Utilities")
+package com.aeolid.generatorproofing
+
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtilRt
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.ex.Range
+import com.intellij.openapi.vcs.ex.createRanges
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
+import com.intellij.util.containers.ContainerUtil
+import java.util.Collections.unmodifiableList
+
+fun getRanges(docText: CharSequence, contentFromVcs: CharSequence): List<Range> {
+	return createRanges(docText, StringUtilRt.convertLineSeparators(contentFromVcs, "\n"))
+}
+
+fun getChangedTextRanges(
+	gotRanges: List<Range>,
+	change: Change?,
+	file: PsiFile,
+	document: Document,
+	contentFromVcs: String?
+): List<TextRange> {
+	return if (change != null && change.type == Change.Type.NEW) {
+		ContainerUtil.newArrayList(file.textRange)
+	} else if (contentFromVcs != null) {
+		getChangedTextRanges(document, gotRanges)
+	} else {
+		emptyList()
+	}
+}
+
+fun getChangedTextRanges(document: Document, changedRanges: List<Range>): List<TextRange> {
+	val ranges = ArrayList<TextRange>()
+
+	for (range in changedRanges) {
+		if (range.hasLines()) {
+			val changeStartLine = range.line1
+			val changeEndLine = range.line2
+
+			val lineStartOffset = document.getLineStartOffset(changeStartLine)
+			val lineEndOffset = document.getLineEndOffset(changeEndLine - 1)
+
+			ranges.add(TextRange(lineStartOffset, lineEndOffset))
+		} else if (range.hasVcsLines()) {
+			val lineEndOffset = document.getLineStartOffset(range.vcsLine1) - 1 // in case of deleted lines, pick correct adjacent element
+			ranges.add(TextRange(lineEndOffset, lineEndOffset))
+		}
+	}
+
+	return ranges
+}
+
+fun getRevisionedContentFrom(change: Change): String? {
+	val revision = change.beforeRevision ?: return null
+
+	return try {
+		revision.content
+	} catch (e: VcsException) {
+		null
+	}
+}
+
+fun getOutsideRanges(text: String, startPattern: String, endPattern: String): List<TextRange> {
+	val ranges = ArrayList<TextRange>()
+	var prevSearchEnd = 0
+	var prevLineEnd = 0
+
+	while (true) {
+		val start = text.indexOf(startPattern, prevSearchEnd)
+		if (start == -1) {
+			ranges.add(TextRange(prevLineEnd, text.length - 1)) // to the end of the file
+			break
+		}
+		val startNextLine = text.indexOf("\n", start) + 1 // skip the start pattern
+		ranges.add(TextRange(prevLineEnd, startNextLine))
+
+		prevSearchEnd = text.indexOf(endPattern, startNextLine)
+		if (prevSearchEnd == -1) {
+			break
+		}
+		prevLineEnd = text.lastIndexOf("\n", prevSearchEnd) + 1 // skip the end pattern indentation
+	}
+
+	return unmodifiableList(ranges)
+}
+
+fun getRelevantRanges(outsideUserSection: List<TextRange>, changes: List<TextRange>): List<TextRange> {
+	val relevantRanges = ArrayList<TextRange>()
+
+	var outsideIdx = 0
+	var changeIdx = 0
+
+	while (outsideIdx < outsideUserSection.size && changeIdx < changes.size) {
+		val outsideRange = outsideUserSection[outsideIdx]
+		val changeRange = changes[changeIdx]
+
+		if (outsideRange.endOffset <= changeRange.startOffset) {
+			outsideIdx++
+		} else if (changeRange.endOffset <= outsideRange.startOffset) {
+			changeIdx++
+		} else {
+			val start = outsideRange.startOffset.coerceAtLeast(changeRange.startOffset)
+			val end = outsideRange.endOffset.coerceAtMost(changeRange.endOffset)
+			relevantRanges.add(TextRange(start, end))
+			if (outsideRange.endOffset < changeRange.endOffset) {
+				outsideIdx++
+			} else {
+				changeIdx++
+			}
+		}
+	}
+
+	return unmodifiableList(relevantRanges)
+}
+
+fun getAffectedRanges(file: PsiFile, startPattern: String, endPattern: String): List<TextRange> {
+	val project = file.project
+	val change = ChangeListManager.getInstance(project).getChange(file.virtualFile)
+	val contentFromVcs = if (change != null) getRevisionedContentFrom(change) else null
+	val document = PsiDocumentManager.getInstance(project).getDocument(file)
+
+	return if (document != null) {
+		val docText = document.charsSequence.toString()
+
+		val outsideUserSection = getOutsideRanges(docText, startPattern, endPattern)
+
+		val changedLines = if (contentFromVcs != null) getRanges(docText, contentFromVcs) else emptyList()
+		val changed = getChangedTextRanges(changedLines, change, file, document, contentFromVcs)
+
+		getRelevantRanges(outsideUserSection, changed)
+	} else {
+		emptyList()
+	}
+}
