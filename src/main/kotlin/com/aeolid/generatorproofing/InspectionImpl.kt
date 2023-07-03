9355b7a5acb9ebf6be3b51fbfe1ac078793b73ff
rainforce15 <rainforce15@gmail.com>
2023-07-03 07:58:39 +0200

    removed specific loading mechanism

--- src/main/kotlin/com/aeolid/generatorproofing/InspectionImpl.kt
+++ src/main/kotlin/com/aeolid/generatorproofing/InspectionImpl.kt
@@ -4,7 +4,6 @@ import com.aeolid.generatorproofing.InspectionBundle.getMessage
 import com.intellij.codeInspection.*
 import com.intellij.codeInspection.ProblemHighlightType.ERROR
 import com.intellij.codeInspection.ui.InspectionOptionsPanel
-import com.intellij.ide.DataManager
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.project.Project
 import com.intellij.psi.JavaElementVisitor
@@ -34,16 +33,14 @@ class InspectionImpl : AbstractBaseJavaLocalInspectionTool() {
 	override fun createOptionsPanel(): JComponent {
 		val panel = InspectionOptionsPanel()
 
-		DataManager.getInstance().dataContextFromFocusAsync.then {
-			panel.add(JLabel(getMessage("inspection.generatedCodePattern.headerPattern")), "cell 0 0")
-			panel.add(TextField(this, "headerPattern", headerPatternKey, getMessage("inspection.generatedCodePattern.headerPatternInfo")), "cell 1 0, growx, pushx")
+		panel.add(JLabel(getMessage("inspection.generatedCodePattern.headerPattern")), "cell 0 0")
+		panel.add(TextField(this, "headerPattern", headerPatternKey, getMessage("inspection.generatedCodePattern.headerPatternInfo")), "cell 1 0, growx, pushx")
 
-			panel.add(JLabel(getMessage("inspection.generatedCodePattern.beginPattern")), "cell 0 1")
-			panel.add(TextField(this, "beginPattern", beginPatternKey, getMessage("inspection.generatedCodePattern.beginPatternInfo")), "cell 1 1, growx, pushx")
+		panel.add(JLabel(getMessage("inspection.generatedCodePattern.beginPattern")), "cell 0 1")
+		panel.add(TextField(this, "beginPattern", beginPatternKey, getMessage("inspection.generatedCodePattern.beginPatternInfo")), "cell 1 1, growx, pushx")
 
-			panel.add(JLabel(getMessage("inspection.generatedCodePattern.endPattern")), "cell 0 2")
-			panel.add(TextField(this, "endPattern", endPatternKey, getMessage("inspection.generatedCodePattern.endPatternInfo")), "cell 1 2, growx, pushx")
-		}
+		panel.add(JLabel(getMessage("inspection.generatedCodePattern.endPattern")), "cell 0 2")
+		panel.add(TextField(this, "endPattern", endPatternKey, getMessage("inspection.generatedCodePattern.endPatternInfo")), "cell 1 2, growx, pushx")
 
 		return panel
 	}
